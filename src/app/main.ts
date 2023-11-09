f49617713eb122921569c77b6f6e6f78da02ea22
BrunoSerpa <serpabruno2005@gmail.com>
2023-11-09 08:04:13 -0300

    Adicionei o chamado do CRUD dos pets

--- src/app/main.ts
+++ src/app/main.ts
@@ -2,10 +2,14 @@ import Entrada from "../io/entrada";
 
 import Empresa from "../modelo/empresa";
 import AlterarCliente from "../negocio/alterarCliente";
+import AlterarPet from "../negocio/alterarPet";
 
 import CadastroCliente from "../negocio/cadastroCliente";
+import CadastroPet from "../negocio/cadastroPet";
 import DeletarCliente from "../negocio/deletarCliente";
+import DeletarPet from "../negocio/deletarPet";
 import ListagemClientes from "../negocio/listagemClientes";
+import ListagemPets from "../negocio/listagemPet";
 import EmpresaTeste from "./empresaTeste";
 
 console.log(`Bem-vindo ao melhor sistema de gerenciamento de pet shops e clínicas veterinarias`)
@@ -31,14 +35,19 @@ while (execucao) {
             console.log("Bem-vindo ao nosso sistema de cadastro!")
             console.log(`O que deseja cadastrar?`);
             console.log(`1 - Cliente`)
-            //console.log(`2 - Pets`)
+            console.log(`2 - Pets`)
             //console.log(`3 - Produtos`)
             //console.log(`4 - Serviços`)
             console.log(`0 - Voltar`)
             opcao = entrada.receberNumero(`Por favor, escolha uma opção: `)
+            let cadastro
             switch(opcao){
                 case 1:
-                    let cadastro = new CadastroCliente(empresa.getClientes)
+                    cadastro = new CadastroCliente(empresa.getClientes)
+                    cadastro.cadastrar()
+                    break
+                case 2:
+                    cadastro = new CadastroPet(empresa.getClientes)
                     cadastro.cadastrar()
                     break
                 case 0:
@@ -52,16 +61,20 @@ while (execucao) {
             console.log("Bem-vindo ao nosso sistema de visualização!")
             console.log(`O que deseja visualizar?`);
             console.log(`1 - Clientes`)
-            //console.log(`2 - Pets`)
+            console.log(`2 - Pets`)
             //console.log(`3 - Produtos`)
             //console.log(`4 - Serviços`)
             console.log(`0 - Voltar`)
             opcao = entrada.receberNumero(`Por favor, escolha uma opções: `)
+            let listagem
             switch(opcao){
                 case 1:
-                    let listagem = new ListagemClientes(empresa.getClientes)
+                    listagem = new ListagemClientes(empresa.getClientes)
                     listagem.listar()
                     break;
+                case 2:
+                    listagem = new ListagemPets(empresa.getClientes)
+                    listagem.listar()
                 case 0:
                     break;
                 default:
@@ -73,14 +86,19 @@ while (execucao) {
             console.log("Bem-vindo ao nosso sistema de edição!")
             console.log(`O que deseja editar?`);
             console.log(`1 - Clientes`)
-            //console.log(`2 - Pets`)
+            console.log(`2 - Pets`)
             //console.log(`3 - Produtos`)
             //console.log(`4 - Serviços`)
             console.log(`0 - Voltar`)
             opcao = entrada.receberNumero(`Por favor, escolha uma opções: `)
+            let editar
             switch(opcao){
                 case 1:
-                    let editar = new AlterarCliente(empresa.getClientes)
+                    editar = new AlterarCliente(empresa.getClientes)
+                    empresa.setClientes(editar.alterar())
+                    break;
+                case 2:
+                    editar = new AlterarPet(empresa.getClientes)
                     empresa.setClientes(editar.alterar())
                     break;
                 case 0:
@@ -94,14 +112,19 @@ while (execucao) {
             console.log("Bem-vindo ao nosso sistema de deletar!")
             console.log(`O que deseja deletar?`);
             console.log(`1 - Clientes`)
-            //console.log(`2 - Pets`)
+            console.log(`2 - Pets`)
             //console.log(`3 - Produtos`)
             //console.log(`4 - Serviços`)
             console.log(`0 - Voltar`)
             opcao = entrada.receberNumero(`Por favor, escolha uma opções: `)
+            let deleta
             switch(opcao){
                 case 1:
-                    let deleta = new DeletarCliente(empresa.getClientes)
+                    deleta = new DeletarCliente(empresa.getClientes)
+                    empresa.setClientes(deleta.deletar())
+                    break;
+                case 2:
+                    deleta = new DeletarPet(empresa.getClientes)
                     empresa.setClientes(deleta.deletar())
                     break;
                 case 0:
