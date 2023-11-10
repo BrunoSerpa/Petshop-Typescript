ef152d19c79b894be9b77d79282634612d8ccc67
BrunoSerpa <serpabruno2005@gmail.com>
2023-11-10 08:42:30 -0300

    Arrumei o constructor

--- src/modelo/servicoConsumidos.ts
+++ src/modelo/servicoConsumidos.ts
@@ -2,11 +2,11 @@ import Pet from "./pet"
 import Servico from "./servico"
 
 export default class ServicoConsumido {
-    public produtoConsumido: Servico
+    public servicoConsumido: Servico
     public dataConsumo: Date
     public pet: Pet
-    constructor(produtoConsumido: Servico, dataConsumo: Date, pet: Pet){
-        this.produtoConsumido = produtoConsumido
+    constructor(servicoConsumido: Servico, dataConsumo: Date, pet: Pet){
+        this.servicoConsumido = servicoConsumido
         this.dataConsumo = dataConsumo
         this.pet= pet
     }
