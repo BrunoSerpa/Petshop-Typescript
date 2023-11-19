2f5e6cfcc52657bf37f5e3d2eec0d293312d0e4d
BrunoSerpa <serpabruno2005@gmail.com>
2023-11-19 17:17:39 -0300

    Arrumei app

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,35 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import './App.css';
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min"
-
 import { BrowserRouter as Router, Routes, Route, useParams, Navigate } from 'react-router-dom';
+
 import Empresa from './modelo/empresa';
-import EmpresaTeste from './app/empresaTeste';
 import Cliente from './modelo/cliente';
 import Produto from './modelo/produto';
 import Servico from './modelo/servico';
 import Pet from './modelo/pet';
+
 import Navbar from './telas/Navbar';
 import ClientesComponent from './telas/VisualizarClientes';
-import CadastrarClienteComponent from './telas/CadastrarCliente';
 import PetsComponent from './telas/VisualizarPets';
 import ProdutosComponent from './telas/VisualizarProdutos';
 import ServicosComponent from './telas/VisualizarServicos';
+
+import ConsumirComponent from './telas/Consumir';
+import CadastrarClienteComponent from './telas/CadastrarCliente';
+import CadastrarProdutosComponent from './telas/CadastrarProduto';
 import CadastrarServicosComponent from './telas/AlterarServico';
 import CadastrarPetsComponent from './telas/CadastrarPet';
 import DestacarClientesComponent from './telas/DestaquesClientes';
 import DestacarProdutosComponent from './telas/DestaquesProdutos';
 import DestacarServicosComponent from './telas/DestaquesServicos';
-import ConsumirComponent from './telas/Consumir';
 import AlterarClienteComponent from './telas/AlterarCliente';
 import AlterarProdutosComponent from './telas/AlterarProduto';
 import AlterarServicosComponent from './telas/AlterarServico';
 import AlterarPetsComponent from './telas/AlterarPet';
 
+import EmpresaTeste from './app/empresaTeste';
 
 export let empresa = new Empresa();
 let empresaTeste = new EmpresaTeste();
@@ -62,18 +65,17 @@ function App() {
   
   return (
     <div className="App">
-      <Navbar />
       <Router>
+        <Navbar />
         <Routes>
-          <Route path="/clientes" element={<ClientesComponent/>} />
-          <Route path="/cadastrar-cliente" element={<CadastrarClienteComponent/>} />
+          <Route index element={<ClientesComponent clientes={clientesState} setClientes={setClientes}/>} />
+          <Route path="/cadastrar-cliente" element={<CadastrarClienteComponent clientes={clientesState} />} />
           <Route path="/alterar-cliente/:index" element={<AlterarClienteWrapper clientes={clientesState}/>} />
           <Route path="/produtos" element={<ProdutosComponent/>} />
-          <Route path="/cadastrar-produto" element={<CadastrarClienteComponent/>} />
+          <Route path="/cadastrar-produto" element={<CadastrarProdutosComponent/>} />
           <Route path="/alterar-produto/:index" element={<AlterarProdutoWrapper produtos={produtosState}/>} />
           <Route path="/servicos" element={<ServicosComponent/>}/>
           <Route path="/cadastrar-servico" element={<CadastrarServicosComponent/>}  />
-          <Route path="/alterar-servico/:index" element={<AlterarServicoWrapper servicos={servicosState}/>} />
           <Route path="/pets" element={<PetsComponent/>} />
           <Route path="/cadastrar-pet" element={<CadastrarPetsComponent/>} />
           <Route path="/alterar-pet/:index:cpf" element={<AlterarPetWrapper pets={petsState}/>} />
@@ -81,6 +83,7 @@ function App() {
           <Route path="/destacar-produtos/" element={<DestacarProdutosComponent/>}/>
           <Route path="/destacar-servicos/" element={<DestacarServicosComponent/>}/>
           <Route path="/consumir" element={<ConsumirComponent/>} />
+          <Route path='*' element={<ClientesComponent clientes={clientesState} setClientes={setClientes}/>} />
         </Routes>
       </Router>
     </div>
@@ -91,12 +94,11 @@ const AlterarClienteWrapper = ({ clientes }: { clientes: Array<Cliente> }) => {
   const { index } = useParams();
   if (index) {
     const indexInt = parseInt(index, 10);
-    return <AlterarClienteComponent/>;
+    return <AlterarClienteComponent clientes={clientes} clienteSelecionado={clientes[indexInt]} posicaoCliente={indexInt} />;
   } else {
     return <Navigate to="/clientes" />;
   }
 };
-
 const AlterarProdutoWrapper = ({ produtos }: { produtos: Array<Produto> }) => {
   const { index } = useParams();
   if (index) {
