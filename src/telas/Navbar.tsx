7fca5b61367e5c45215e8730eb0467ef9a96bd39
BrunoSerpa <serpabruno2005@gmail.com>
2023-11-23 07:42:25 -0300

    Arrumei o navbar

--- src/telas/Navbar.tsx
+++ src/telas/Navbar.tsx
@@ -6,13 +6,25 @@ const Navbar = () => {
             <nav className="navbar navbar-expand-lg" data-bs-theme="light">
                 <div className="container-fluid">
                     <span className="navbar-brand mb-0 h1">PetLovers</span>
-                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <a
+                        className="navbar-toggler"
+                        type="button" data-bs-toggle="collapse"
+                        data-bs-target="#navbarNav"
+                        aria-controls="navbarNav"
+                        aria-expanded="false"
+                        aria-label="Toggle navigation"
+                    >
                         <span className="navbar-toggler-icon"></span>
-                    </button>
+                    </a>
                     <div className="collapse navbar-collapse" id="navbarNav">
                         <ul className="navbar-nav">
                             <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <a
+                                    className="nav-link dropdown-toggle"
+                                    role="button"
+                                    data-bs-toggle="dropdown"
+                                    aria-expanded="false"
+                                >
                                     Cadastrar
                                 </a>
                                 <ul className="dropdown-menu">
@@ -24,16 +36,26 @@ const Navbar = () => {
                                 </ul>
                             </li>
                             <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <a
+                                    className="nav-link dropdown-toggle"
+                                    role="button"
+                                    data-bs-toggle="dropdown"
+                                    aria-expanded="false"
+                                >
                                     Consumir
                                 </a>
                                 <ul className="dropdown-menu">
-                                    <li><Link to="/consumir-produtos" className="dropdown-item">Produtos</Link></li>
-                                    <li><Link to="/consumir-servicos" className="dropdown-item">Serviços</Link></li>
+                                    <li><Link to="/consumir-produto" className="dropdown-item">Produtos</Link></li>
+                                    <li><Link to="/consumir-servico" className="dropdown-item">Serviços</Link></li>
                                 </ul>
                             </li>
                             <li className="nav-item dropdown">
-                                <a className="nav-link dropdown-toggle"role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                <a
+                                    className="nav-link dropdown-toggle"
+                                    role="button"
+                                    data-bs-toggle="dropdown"
+                                    aria-expanded="false"
+                                >
                                     Visualisar
                                 </a>
                                 <ul className="dropdown-menu">
@@ -42,6 +64,21 @@ const Navbar = () => {
                                     <li><hr className="dropdown-divider"/></li>
                                     <li><Link to="/produtos" className="dropdown-item">Produtos</Link></li>
                                     <li><Link to="/servicos" className="dropdown-item">Serviços</Link></li>
+                                </ul>
+                            </li>
+                            <li className="nav-item dropdown">
+                                <a
+                                    className="nav-link dropdown-toggle"
+                                    role="button"
+                                    data-bs-toggle="dropdown"
+                                    aria-expanded="false"
+                                >
+                                    Destacar
+                                </a>
+                                <ul className="dropdown-menu">
+                                    <li><Link to="/destacar-clientes" className="dropdown-item">Clientes</Link></li>
+                                    <li><Link to="/destacar-produtos" className="dropdown-item">Produtos</Link></li>
+                                    <li><Link to="/destacar-servicos" className="dropdown-item">Servicos</Link></li>
                                 </ul>
                             </li>
                         </ul>
