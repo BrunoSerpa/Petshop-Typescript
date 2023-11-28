f3a88c021e789259a8ab0c9e6666ee29e06c45dc
BrunoSerpa <serpabruno2005@gmail.com>
2023-11-28 08:57:46 -0300

    Arrumei os consumos

--- src/negocio/consumirProduto.ts
+++ src/negocio/consumirProduto.ts
@@ -4,58 +4,30 @@ import Empresa from "../modelo/empresa";
 
 import Pet from "../modelo/pet";
 import Produto from "../modelo/produto";
-import ProdutoConsumido from "../modelo/produtoConsumidos";
+import ProdutoConsumido from "../modelo/produtoConsumido";
+import { buscarPet } from "./funcoesPet";
+import { buscaProduto } from "./funcoesProdutos";
 
-import ListagemClientes from "./listagemClientes";
-import ListagemProdutos from "./listarProdutos";
 
 export default class ConsumirProduto{
     private empresa: Empresa
     private entrada: Entrada
-
-    private listaClientes
-    private listarProdutos
-
     private clienteEscolhido!: Cliente
     private petEscolhido!: Pet
     private produtoEscolhido!: Produto
     constructor(empresa: Empresa) {
         this.entrada = new Entrada()
         this.empresa = empresa
-        this.listaClientes =  new ListagemClientes(this.empresa.getClientes)
-        this.listarProdutos =  new ListagemProdutos(this.empresa.getProdutos)
     }
     public get consumir(): Empresa{
         while (true){
-            const clienteSelecionado= this.listaClientes.selecionarCliente
-            if (clienteSelecionado){
-                while (true){
-                    let quantPets:number 
-                    quantPets=clienteSelecionado.getPets.length
-                    console.log(`Escolha o pet que fará o serviço`)
-                    let numPets=[]
-                    for (let i = 1; i < quantPets+1; i++) {
-                        console.log(`${i} - ${i}º pet`)
-                        numPets.push(i)
-                    }
-                    console.log(`0 - Cancelar`)
-                    let petEscolhido = this.entrada.receberNumero(`Por favor, informe uma das opções:`)
-                    if (numPets.includes(petEscolhido)){
-                        this.petEscolhido = clienteSelecionado.getPets[petEscolhido -1]
-                    }
-                    else if (petEscolhido !== 0){
-                        console.log(`Operação não entendida :(`)
-                        continue
-                    }
-                    break
-                }
-                const produtoSelecionado= this.listarProdutos.selecionarProduto
+            const clienteSelecionado= buscarPet(this.empresa.getClientes, this.entrada, true)
+            if (clienteSelecionado instanceof Array){
+                this.petEscolhido = clienteSelecionado[0]
+                this.clienteEscolhido = clienteSelecionado[1]
+                const produtoSelecionado= buscaProduto(this.empresa.getProdutos, this.entrada)
                 if (produtoSelecionado){
-                    this.produtoEscolhido=produtoSelecionado
-                } else {
-                    break
-                }
-                if (this.clienteEscolhido && this.petEscolhido && this.produtoEscolhido){
+                    this.produtoEscolhido = produtoSelecionado
                     let data = new Date()
                     const produtoConsumido = new ProdutoConsumido(this.produtoEscolhido, data, this.petEscolhido)
                     this.clienteEscolhido.getProdutosConsumidos.push(produtoConsumido)
@@ -71,6 +43,7 @@ export default class ConsumirProduto{
             if (continuar.toUpperCase() === 'S') {
                 continue
             }
+            break
         }
         return this.empresa
     }
--- src/negocio/consumirServico.ts
+++ src/negocio/consumirServico.ts
@@ -4,58 +4,30 @@ import Empresa from "../modelo/empresa";
 
 import Pet from "../modelo/pet";
 import Servico from "../modelo/servico";
-import ServicoConsumido from "../modelo/servicoConsumidos";
+import ServicoConsumido from "../modelo/servicoConsumido";
+import { buscarPet } from "./funcoesPet";
+import { buscaServico } from "./funcoesServicos";
 
-import ListagemClientes from "./listagemClientes";
-import ListagemServicos from "./listarServicos";
 
 export default class ConsumirServico{
     private empresa: Empresa
     private entrada: Entrada
-
-    private listaClientes
-    private listarServicos
-
     private clienteEscolhido!: Cliente
     private petEscolhido!: Pet
     private servicoEscolhido!: Servico
     constructor(empresa: Empresa) {
         this.entrada = new Entrada()
         this.empresa = empresa
-        this.listaClientes =  new ListagemClientes(this.empresa.getClientes)
-        this.listarServicos =  new ListagemServicos(this.empresa.getServicos)
     }
     public get consumir(): Empresa{
         while (true){
-            const clienteSelecionado= this.listaClientes.selecionarCliente
-            if (clienteSelecionado){
-                while (true){
-                    let quantPets:number 
-                    quantPets=clienteSelecionado.getPets.length
-                    console.log(`Escolha o pet que fará o serviço`)
-                    let numPets=[]
-                    for (let i = 1; i < quantPets+1; i++) {
-                        console.log(`${i} - ${i}º pet`)
-                        numPets.push(i)
-                    }
-                    console.log(`0 - Cancelar`)
-                    let petEscolhido = this.entrada.receberNumero(`Por favor, informe uma das opções:`)
-                    if (numPets.includes(petEscolhido)){
-                        this.petEscolhido = clienteSelecionado.getPets[petEscolhido -1]
-                    }
-                    else if (petEscolhido !== 0){
-                        console.log(`Operação não entendida :(`)
-                        continue
-                    }
-                    break
-                }
-                const servicoSelecionado= this.listarServicos.selecionarServico
+            const clienteSelecionado= buscarPet(this.empresa.getClientes, this.entrada, true)
+            if (clienteSelecionado instanceof Array){
+                this.petEscolhido = clienteSelecionado[0]
+                this.clienteEscolhido = clienteSelecionado[1]
+                const servicoSelecionado= buscaServico(this.empresa.getServicos, this.entrada)
                 if (servicoSelecionado){
-                    this.servicoEscolhido=servicoSelecionado
-                } else {
-                    break
-                }
-                if (this.clienteEscolhido && this.petEscolhido && this.servicoEscolhido){
+                    this.servicoEscolhido = servicoSelecionado
                     let data = new Date()
                     const servicoConsumido = new ServicoConsumido(this.servicoEscolhido, data, this.petEscolhido)
                     this.clienteEscolhido.getServicosConsumidos.push(servicoConsumido)
@@ -71,6 +43,7 @@ export default class ConsumirServico{
             if (continuar.toUpperCase() === 'S') {
                 continue
             }
+            break
         }
         return this.empresa
     }
