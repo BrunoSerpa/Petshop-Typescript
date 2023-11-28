f2f63e3b0b8424530d2ae32dfec6f06a6461a9bd
BrunoSerpa <serpabruno2005@gmail.com>
2023-11-28 08:59:07 -0300

    Arrumei a main para a nova estrutura

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,27 +1,13 @@
-import Entrada from "../io/entrada";
-
-import Empresa from "../modelo/empresa";
-import AlterarCliente from "../negocio/alterarCliente";
-import AlterarPet from "../negocio/alterarPet";
-import AlterarProdutos from "../negocio/alterarProduto";
-import AlterarServicos from "../negocio/alterarServico";
-
-import CadastroCliente from "../negocio/cadastroCliente";
-import CadastroPet from "../negocio/cadastroPet";
-import CadastroProduto from "../negocio/cadastroProduto";
-import CadastroServico from "../negocio/cadastroServico";
-import ConsumirProduto from "../negocio/consumirProduto";
-import ConsumirServicos from "../negocio/consumirServico";
-import DeletarCliente from "../negocio/deletarCliente";
-import DeletarPet from "../negocio/deletarPet";
-import DeletarProduto from "../negocio/deletarProduto";
-import DeletarServico from "../negocio/deletarServico";
-import DestacarClientes from "../negocio/destacarClientes";
-import ListagemClientes from "../negocio/listagemClientes";
-import ListagemPets from "../negocio/listagemPets";
-import ListagemProdutos from "../negocio/listarProdutos";
-import ListagemServicos from "../negocio/listarServicos";
-import EmpresaTeste from "./empresaTeste";
+import Entrada from "../io/entrada"
+import Empresa from "../modelo/empresa"
+import ConsumirProduto from "../negocio/consumirProduto"
+import ConsumirServico from "../negocio/consumirServico"
+import Destacar from "../negocio/funcoesDestaques"
+import { AlteracaoCliente, CadastroCliente, ExclusaoCliente, ListaCliente } from "../negocio/funcoesCliente"
+import { AlteracaoPet, CadastroPet, ExclusaoPet, ListaPet } from "../negocio/funcoesPet"
+import { AlteracaoProduto, CadastroProduto, ExclusaoProduto, ListaProdutos } from "../negocio/funcoesProdutos"
+import { AlteracaoServico, CadastroServico, ExclusaoServico, ListaServicos } from "../negocio/funcoesServicos"
+import EmpresaTeste from "./empresaTeste"
 
 console.log(`Seja Bem-vindo!!`)
 let entrada = new Entrada()
@@ -30,14 +16,14 @@ let empresaTeste = new EmpresaTeste()
 const clientes = empresaTeste.clientesEmpresaTeste()
 const produtos = empresaTeste.produtosEmpresaTeste()
 const servicos = empresaTeste.servicosEmpresaTeste()
-empresa.setClientes=clientes
-empresa.setProdutos=produtos
-empresa.setServicos=servicos
+empresa.setClientes = clientes
+empresa.setProdutos = produtos
+empresa.setServicos = servicos
 empresaTeste.produtosEServicosConsumidosTeste()
 
-let continuar:boolean = true
-let opcao:number = 0
-let resposta:string = ''
+let continuar: boolean = true
+let opcao: number = 0
+let resposta: string = ''
 
 while (continuar) {
     console.log(`O que deseja fazer?`);
@@ -51,7 +37,7 @@ while (continuar) {
     opcao = entrada.receberNumero(`Por favor, escolha uma opção: `)
     switch (opcao) {
         case 1:
-            while (continuar){
+            while (continuar) {
                 console.log(`O que deseja cadastrar?`);
                 console.log(`1 - Clientes`)
                 console.log(`2 - Pets`)
@@ -59,23 +45,18 @@ while (continuar) {
                 console.log(`4 - Serviços`)
                 console.log(`0 - Voltar`)
                 opcao = entrada.receberNumero(`Por favor, escolha uma opção: `)
-                let cadastro
-                switch(opcao){
+                switch (opcao) {
                     case 1:
-                        cadastro = new CadastroCliente(empresa.getClientes)
-                        cadastro.cadastrar
+                        new CadastroCliente(empresa.getClientes).cadastrar()
                         break
                     case 2:
-                        cadastro = new CadastroPet(empresa.getClientes)
-                        cadastro.cadastrar
+                        new CadastroPet(empresa.getClientes).cadastrar()
                         break
                     case 3:
-                        cadastro = new CadastroProduto(empresa.getProdutos)
-                        cadastro.cadastrar()
+                        new CadastroProduto(empresa.getProdutos).cadastrar()
                         break
                     case 4:
-                        cadastro = new CadastroServico(empresa.getServicos)
-                        cadastro.cadastrar()
+                        new CadastroServico(empresa.getServicos).cadastrar()
                         break
                     case 0:
                         break
@@ -83,9 +64,9 @@ while (continuar) {
                         console.log(`Operação não entendida :(`)
                         continue
                 }
-                if ([1,2,3,4].includes(opcao)){
+                if ([1, 2, 3, 4].includes(opcao)) {
                     resposta = entrada.receberTexto(`Deseja cadastrar mais alguma coisa? (S/N)`)
-                    if (resposta.toUpperCase() === 'S'){
+                    if (resposta.toUpperCase() === 'S') {
                         continue
                     }
                 }
@@ -94,7 +75,7 @@ while (continuar) {
             continuar = true
             break;
         case 2:
-            while (continuar){
+            while (continuar) {
                 console.log(`O que deseja visualizar?`);
                 console.log(`1 - Clientes`)
                 console.log(`2 - Pets`)
@@ -102,23 +83,18 @@ while (continuar) {
                 console.log(`4 - Serviços`)
                 console.log(`0 - Voltar`)
                 opcao = entrada.receberNumero(`Por favor, escolha uma opções: `)
-                let listagem
-                switch(opcao){
+                switch (opcao) {
                     case 1:
-                        listagem = new ListagemClientes(empresa.getClientes)
-                        listagem.listar()
+                        new ListaCliente(empresa.getClientes).listarClientes()
                         break;
                     case 2:
-                        listagem = new ListagemPets(empresa.getClientes)
-                        listagem.listar()
+                        new ListaPet(empresa.getClientes).listar()
                         break
                     case 3:
-                        listagem = new ListagemProdutos(empresa.getProdutos)
-                        listagem.listar()
+                        new ListaProdutos(empresa.getProdutos).listar()
                         break
                     case 4:
-                        listagem = new ListagemServicos(empresa.getServicos)
-                        listagem.listar()
+                        new ListaServicos(empresa.getServicos).listar()
                         break
                     case 0:
                         break;
@@ -126,9 +102,9 @@ while (continuar) {
                         console.log(`Operação não entendida :(`)
                         continue
                 }
-                if ([1,2,3,4].includes(opcao)){
+                if ([1, 2, 3, 4].includes(opcao)) {
                     resposta = entrada.receberTexto(`Deseja listar mais alguma coisa? (S/N)`)
-                    if (resposta.toUpperCase() === 'S'){
+                    if (resposta.toUpperCase() === 'S') {
                         continue
                     }
                 }
@@ -137,7 +113,7 @@ while (continuar) {
             continuar = true
             break;
         case 3:
-            while (continuar){
+            while (continuar) {
                 console.log(`O que deseja alterar?`);
                 console.log(`1 - Clientes`)
                 console.log(`2 - Pets`)
@@ -145,23 +121,18 @@ while (continuar) {
                 console.log(`4 - Serviços`)
                 console.log(`0 - Voltar`)
                 opcao = entrada.receberNumero(`Por favor, escolha uma opções: `)
-                let editar
-                switch(opcao){
+                switch (opcao) {
                     case 1:
-                        editar = new AlterarCliente(empresa.getClientes)
-                        empresa.setClientes=editar.alterar
+                        new AlteracaoCliente(empresa.getClientes).alterarCliente()
                         break;
                     case 2:
-                        editar = new AlterarPet(empresa.getClientes)
-                        empresa.setClientes=editar.alterar
+                        new AlteracaoPet(empresa.getClientes).alterar()
                         break;
                     case 3:
-                        editar = new AlterarProdutos(empresa.getProdutos)
-                        empresa.setProdutos=editar.alterar
+                        new AlteracaoProduto(empresa.getProdutos).alterar()
                         break;
                     case 4:
-                        editar = new AlterarServicos(empresa.getServicos)
-                        empresa.setServicos=editar.alterar
+                        new AlteracaoServico(empresa.getServicos).alterar()
                         break;
                     case 0:
                         break;
@@ -169,9 +140,9 @@ while (continuar) {
                         console.log(`Operação não entendida :(`)
                         continue
                 }
-                if ([1, 2, 3 , 4].includes(opcao)){
+                if ([1, 2, 3, 4].includes(opcao)) {
                     resposta = entrada.receberTexto(`Deseja alterar mais alguma coisa? (S/N)`)
-                    if (resposta.toUpperCase() === 'S'){
+                    if (resposta.toUpperCase() === 'S') {
                         continue
                     }
                 }
@@ -180,7 +151,7 @@ while (continuar) {
             continuar = true
             break;
         case 4:
-            while (continuar){
+            while (continuar) {
                 console.log(`O que deseja deletar?`);
                 console.log(`1 - Clientes`)
                 console.log(`2 - Pets`)
@@ -188,23 +159,18 @@ while (continuar) {
                 console.log(`4 - Serviços`)
                 console.log(`0 - Voltar`)
                 opcao = entrada.receberNumero(`Por favor, escolha uma opções: `)
-                let deleta
-                switch(opcao){
+                switch (opcao) {
                     case 1:
-                        deleta = new DeletarCliente(empresa.getClientes)
-                        empresa.setClientes=deleta.deletar
+                        empresa.setClientes = new ExclusaoCliente(empresa.getClientes).deletar()
                         break;
                     case 2:
-                        deleta = new DeletarPet(empresa.getClientes)
-                        empresa.setClientes=deleta.deletar
+                        new ExclusaoPet(empresa.getClientes).deletar()
                         break;
                     case 3:
-                        deleta = new DeletarProduto(empresa.getProdutos)
-                        empresa.setProdutos=deleta.deletar
+                        new ExclusaoProduto(empresa.getProdutos).deletar()
                         break;
                     case 4:
-                        deleta = new DeletarServico(empresa.getServicos)
-                        empresa.setServicos=deleta.deletar
+                        new ExclusaoServico(empresa.getServicos).deletar()
                         break;
                     case 0:
                         break;
@@ -212,9 +178,9 @@ while (continuar) {
                         console.log(`Operação não entendida :(`)
                         continue
                 }
-                if ([1, 2, 3 , 4].includes(opcao)){
+                if ([1, 2, 3, 4].includes(opcao)) {
                     resposta = entrada.receberTexto(`Deseja excluir mais alguma coisa? (S/N)`)
-                    if (resposta.toUpperCase() === 'S'){
+                    if (resposta.toUpperCase() === 'S') {
                         continue
                     }
                 }
@@ -223,21 +189,21 @@ while (continuar) {
             continuar = true
             break;
         case 5:
-            while (continuar){
+            while (continuar) {
                 console.log(`O que deseja consumir?`);
                 console.log(`1 - Produtos`)
                 console.log(`2 - Serviços`)
                 console.log(`0 - Voltar`)
                 opcao = entrada.receberNumero(`Por favor, escolha uma opção: `)
                 let consumo
-                switch(opcao){
+                switch (opcao) {
                     case 1:
                         consumo = new ConsumirProduto(empresa)
-                        empresa=consumo.consumir
+                        empresa = consumo.consumir
                         break
                     case 2:
-                        consumo = new ConsumirServicos(empresa)
-                        empresa=consumo.consumir
+                        consumo = new ConsumirServico(empresa)
+                        empresa = consumo.consumir
                         break
                     case 0:
                         break
@@ -245,9 +211,9 @@ while (continuar) {
                         console.log(`Operação não entendida :(`)
                         continue
                 }
-                if ([1, 2].includes(opcao)){
+                if ([1, 2].includes(opcao)) {
                     resposta = entrada.receberTexto(`Deseja consumir mais alguma coisa? (S/N)`)
-                    if (resposta.toUpperCase() === 'S'){
+                    if (resposta.toUpperCase() === 'S') {
                         continue
                     }
                 }
@@ -256,7 +222,7 @@ while (continuar) {
             continuar = true
             break
         case 6:
-            while (continuar){
+            while (continuar) {
                 console.log(`O que deseja visualizar?`)
                 console.log(`1 - Clientes que mais consumiram produtos (quantidade)`)
                 console.log(`2 - Clientes que mais consumiram serviços (quantidade)`)
@@ -266,42 +232,14 @@ while (continuar) {
                 console.log(`6 - Clientes que mais consumiram serviços (valor)`)
                 console.log(`0 - Voltar`)
                 opcao = entrada.receberNumero(`Por favor, escolha uma opção: `)
-                let destacar = new DestacarClientes(empresa.getClientes)
-                switch(opcao){
-                    case 1:
-                        destacar.destacar(opcao)
-                        break
-                    case 2:
-                        destacar = new DestacarClientes(empresa.getClientes)
-                        destacar.destacar(opcao)
-                        break
-                    case 3:
-                        destacar = new DestacarClientes(empresa.getClientes)
-                        destacar.destacar(opcao)
-                        break
-                    case 4:
-                        destacar = new DestacarClientes(empresa.getClientes)
-                        destacar.destacar(opcao)
-                        break
-                    case 5:
-                        destacar = new DestacarClientes(empresa.getClientes)
-                        destacar.destacar(5)
-                        break
-                    case 6:
-                        destacar = new DestacarClientes(empresa.getClientes)
-                        destacar.destacar(6)
-                        break
-                    case 0:
-                        break
-                    default:
-                        console.log(`Operação não entendida :(`)
-                        continue
-                }
-                if ([1, 2, 3, 4, 5, 6].includes(opcao)){
+                if ([1,2,3,4,5,6].includes(opcao)){
+                    new Destacar(empresa.getClientes).destacar(opcao)
                     resposta = entrada.receberTexto(`Deseja visualizar mais alguma coisa? (S/N)`)
-                    if (resposta.toUpperCase() === 'S'){
+                    if (resposta.toUpperCase() === 'S') {
                         continue
                     }
+                } else if (opcao !== 0){
+                    console.log(`Operação não entendida :(`)
                 }
                 continuar = false
             }
